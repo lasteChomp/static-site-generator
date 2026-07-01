da86939a5f0c71628b005f6a0b3340f84427f8e4
lasteChomp <163031341+lasteChomp@users.noreply.github.com>
2026-07-01 17:56:15 +0300

    feat: add split_nodes_image and split_nodes_link functions

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -26,3 +26,53 @@ def extract_markdown_images(text: str) -> list[tuple[str, str]]:
 
 def extract_markdown_links(text: str) -> list[tuple[str, str]]:
     return findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes: list[TextNode] = []
+    for node in old_nodes:
+        if node.text_type != TextType.PLAIN_TEXT:
+            new_nodes.append(node)
+            continue
+        extracted_markdown_image = extract_markdown_images(node.text)
+        if len(extracted_markdown_image) == 0:
+            new_nodes.append(node)
+            continue
+        for image in extracted_markdown_image:
+            image_alt = image[0]
+            image_url = image[1]
+            parts = node.text.split(f"![{image_alt}]({image_url})", maxsplit=1)
+            if len(parts) != 2:
+                raise ValueError("invalid markdown, image section not closed")
+            if parts[0] != "":
+                new_nodes.append(TextNode(parts[0], TextType.PLAIN_TEXT))
+            new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_url))
+            node.text = parts[1]
+        if node.text != "":
+            new_nodes.append(TextNode(node.text, TextType.PLAIN_TEXT))
+    return new_nodes
+
+def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes: list[TextNode] = []
+    for node in old_nodes:
+        if node.text_type != TextType.PLAIN_TEXT:
+            new_nodes.append(node)
+            continue
+        extracted_markdown_link = extract_markdown_links(node.text)
+        if len(extracted_markdown_link) == 0:
+            new_nodes.append(node)
+            continue
+        for link in extracted_markdown_link:
+            link_alt = link[0]
+            link_url = link[1]
+            parts = node.text.split(f"[{link_alt}]({link_url})", maxsplit=1)
+            if len(parts) != 2:
+                raise ValueError("invalid markdown, link section not closed")
+            if parts[0] != "":
+                new_nodes.append(TextNode(parts[0], TextType.PLAIN_TEXT))
+            new_nodes.append(TextNode(link_alt, TextType.LINK, link_url))
+            node.text = parts[1]
+        if node.text != "":
+            new_nodes.append(TextNode(node.text, TextType.PLAIN_TEXT))
+    return new_nodes
+
+            
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
 
 class TestInlineMarkdown(unittest.TestCase):
     def test_split_delimeter_plain_text(self):
@@ -126,3 +126,39 @@ class TestInlineMarkdown(unittest.TestCase):
             ("to boot dev", "https://www.boot.dev"),
             ("to youtube", "https://www.youtube.com/@bootdotdev")
         ], matches)
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png) and also another ![github](https://github.com/lasteChomp/static-site-generator)",
+            TextType.PLAIN_TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.PLAIN_TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.PLAIN_TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+                TextNode(" and also another ", TextType.PLAIN_TEXT),
+                TextNode("github", TextType.IMAGE, "https://github.com/lasteChomp/static-site-generator")
+            ],
+            new_nodes,
+        )
+
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.PLAIN_TEXT
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a link ", TextType.PLAIN_TEXT),
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode(" and ", TextType.PLAIN_TEXT),
+                TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev") 
+            ],
+            new_nodes
+        )
