ba4f8f35ee39d962422fc27c7738b632234b844c
lasteChomp <163031341+lasteChomp@users.noreply.github.com>
2026-07-08 18:16:13 +0300

    refactor: move path constants to module scope and use os.path.join for path construction

--- src/main.py
+++ src/main.py
@@ -1,15 +1,19 @@
+import os
 from copy_static import copy_static_to_public
 from generate_page import generate_page
 
+source_path = "./static"
+destination_path = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 def main():
-    source_path = "./static"
-    destination_path = "./public"
-    from_path = "./content/index.md"
-    template_path = "./template.html"
-    dest_path = "./public/index.html"
     copying_operation = copy_static_to_public(source_path, destination_path)
-    generating_page_operation = generate_page(from_path, template_path, dest_path)
+    generating_page_operation = generate_page(
+        os.path.join(dir_path_content, "index.md"), 
+        template_path, 
+        os.path.join(destination_path, "index.html"),
+    )
     print(copying_operation)
     print(generating_page_operation)
 
